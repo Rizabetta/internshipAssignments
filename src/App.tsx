93ed831e0247f6378c9f66aa3a4f48879646551f
Rizabetta <liza.popechiteleva@mail.ru>
2023-12-26 13:33:50 +0300

    refactor: visualization of query data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Methods, requests } from "./api/universal";
+import { Table } from "./components";
 
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    requests.universal(Methods.GET, "1/comments").then((r) => (setPosts(r.data)));
+  }, []);
+
+  return (
+     <Table posts={posts}></Table>
+  );
 }
 
 export default App;
