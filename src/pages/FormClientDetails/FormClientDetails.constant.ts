04835a3aa039fedcc0db0c112f616b7b8dd3fde1
Rizabetta <liza.popechiteleva@mail.ru>
2024-01-15 15:25:49 +0300

    refactor: changing ClientDetails parameters and getting data for select from jsonplaceholder

--- src/pages/FormClientDetails/FormClientDetails.constant.ts
+++ src/pages/FormClientDetails/FormClientDetails.constant.ts
@@ -1,44 +1,98 @@
+import {
+  DatePicker,
+  Input,
+  InputNumber,
+  RangePicker,
+  Select,
+} from "../../components";
+import { Methods, requests } from "../../api/api.config";
+
+export type TCityOptions = { value: string; label: string }[] | undefined;
+export type TCity = {
+  address: {
+    city: string;
+  };
+};
+
+const result = await requests.makeApiRequest(Methods.GET, "/users");
+const mappedResult = result.data.map((obj: TCity) => {
+  return {
+    value: obj.address.city,
+    label: obj.address.city,
+  };
+});
+
 const clientDetails = [
   {
-    type: "Input",
-    name: "Фамилия",
+    id: 1,
     label: "Фамилия",
+    name: "Surname",
     required: true,
-    message: "Введите свою фамилию",
+    placeholder: "Введите свою фамилию",
+    component: Input,
   },
   {
-    type: "Input",
-    name: "Имя",
+    id: 2,
     label: "Имя",
+    name: "Name",
     required: true,
-    message: "Введите свое имя",
+    placeholder: "Введите свое имя",
+    component: Input,
   },
   {
-    type: "Input",
-    name: "Отчество",
+    id: 3,
     label: "Отчество",
+    name: "MiddleName",
     required: true,
-    message: "Введите свое отчество",
+    placeholder: "Введите свое отчество",
+    component: Input,
   },
   {
-    type: "DatePicker",
+    id: 4,
     label: "День рождения",
+    name: "Birthday",
     required: true,
-    message: "Выберите дату рождения",
+    placeholder: "Выберите дату рождения",
+    component: DatePicker,
   },
   {
-    type: "RangePicker",
+    id: 5,
     label: "Даты начала и окончания обучения",
+    name: "DatesTraining",
     required: true,
-    message: "Выберите дату начала и окончания обучения",
+    rangePickerPlaceholder: ["Дата начала", "Дата окончания"],
+    component: RangePicker,
   },
   {
-    type: "InputNumber",
+    id: 6,
     label: "Номер телефона",
+    name: "PhoneNumber",
     required: true,
-    message: "Введите номер телефона",
+    placeholder: "Введите номер телефона",
+    component: InputNumber,
+    addonBefore: "+7",
+    maxLength: 10,
+    minLength: 10,
   },
-  { type: "InputNumber", label: "ИНН", required: true, message: "Выберите ИНН" },
+  {
+    id: 7,
+    label: "ИНН",
+    name: "ITN",
+    required: true,
+    placeholder: "Введите ИНН",
+    component: InputNumber,
+    maxLength: 12,
+    minLength: 12,
+  },
+  {
+    id: 8,
+    label: "Город",
+    name: "City",
+    required: true,
+    component: Select,
+    options: mappedResult,
+    placeholder: "Выберите город",
+  },
 ];
 
 export { clientDetails };
