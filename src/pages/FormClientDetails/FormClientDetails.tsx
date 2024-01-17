4ec9dced80b3923e3c21b439a5cf27908b8530b4
Rizabetta <liza.popechiteleva@mail.ru>
2024-01-17 09:30:00 +0300

    refactor: applying styles, wrapping in FormWrapper, typing placeholder

--- src/pages/FormClientDetails/FormClientDetails.tsx
+++ src/pages/FormClientDetails/FormClientDetails.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { Button } from "antd";
 import { nanoid } from "nanoid";
-import { Modal } from "../../components";
+import style from "./FormClientDetails.module.scss";
+import { FormWrapper, Modal } from "../../components";
 import { clientDetails } from "./FormClientDetails.constant";
 
 const FormClientDetails = () => {
@@ -42,48 +43,60 @@ const FormClientDetails = () => {
   };
 
   return (
-    <main>
-      <h1>Сведения о клиенте</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {clientDetails.map(
-          ({ label, name, component, ...props }) => (
-            <div key={nanoid()}>
-              <label htmlFor={name}>{label}</label>
-              <Controller
-                control={control}
-                name={name as keyof TFormData}
-                render={({ field }) => {
-                  const { ref, ...rest } = field;
-                  const FieldComponent = component;
-                  return <FieldComponent {...rest} {...props} />;
-                }}
-              />
-            </div>
-          )
-        )}
+    <section className={style.container}>
+      <FormWrapper>
+        <h1>Сведения о клиенте</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {clientDetails.map(
+            ({ label, name, placeholder, component, ...props }) => (
+              <div className={style.formItem} key={nanoid()}>
+                <p className={style.itemName}>{label}</p>
+                <Controller
+                  control={control}
+                  name={name as keyof TFormData}
+                  render={({ field }) => {
+                    const { ref, ...rest } = field;
+                    const FieldComponent = component;
+                    return (
+                      <FieldComponent
+                        placeholder={
+                          placeholder as (string & [string, string]) | undefined
+                        }
+                        {...rest}
+                        {...props}
+                      />
+                    );
+                  }}
+                />
+              </div>
+            )
+          )}
 
-        <button type="submit">Сохранить</button>
+          <button className={style.btnSubmit} type="submit">
+            Сохранить
+          </button>
 
-        <Modal
-          title={"Вы хотите очистить форму?"}
-          open={open}
-          handleOk={handleOk}
-          handleCancel={handleCancel}
-        >
-          <Button key="back" onClick={handleCancel}>
-            Нет
-          </Button>
-          <Button
-            key="submit"
-            type="primary"
-            loading={loading}
-            onClick={handleOk}
+          <Modal
+            title={"Вы хотите очистить форму?"}
+            open={open}
+            handleOk={handleOk}
+            handleCancel={handleCancel}
           >
-            Да
-          </Button>
-        </Modal>
-      </form>
-    </main>
+            <Button key="back" onClick={handleCancel}>
+              Нет
+            </Button>
+            <Button
+              key="submit"
+              type="primary"
+              loading={loading}
+              onClick={handleOk}
+            >
+              Да
+            </Button>
+          </Modal>
+        </form>
+      </FormWrapper>
+    </section>
   );
 };
 
